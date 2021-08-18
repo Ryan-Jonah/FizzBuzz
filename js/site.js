8c075c07c7c22de999642b61d9effbeb9aa5e2c7
Ryan J <76914994+RyanJ-NBCC@users.noreply.github.com>
2021-08-18 16:54:42 -0300

    Updated Styling

--- js/site.js
+++ js/site.js
@@ -1,3 +1,62 @@
-function helloWorld(){
-    alert("Hello World!");                                
+/**
+ * Gather user input from form controls
+ * Controller Function
+ */
+function getValues(){
+    let fizzValue = parseInt(document.getElementById('fizzValue').value);
+    let buzzValue = parseInt(document.getElementById('buzzValue').value);
+
+    if (isNaN(fizzValue) == false && isNaN(buzzValue) == false){
+        
+        let fizzNumbers = [];
+        let buzzNumbers = [];
+        let fizzBuzzNumbers = [];
+
+        displayResults(fizzNumbers, buzzNumbers, fizzBuzzNumbers);
+    }
+}
+
+/**
+ * Determine multiples of 3, 5, and 3 + 5
+ * Logic Functions
+ */
+function getFizzNumbers(fizz, buzz){
+    let returnNumbers = [];
+
+    for (let index = 0; index <= 100; index++) {
+        if (index % fizz == 0){
+            returnNumbers.push(index);
+        }
+    }
+    return returnNumbers;
+}
+
+function getBuzzNumbers(fizz, buzz){
+    let returnNumbers = [];
+
+    for (let index = 0; index <= 100; index++) {
+        if (index % buzz == 0){
+            returnNumbers += index;
+        }
+    }
+    return returnNumbers;
+}
+
+function getFizzBuzzNumbers(fizz, buzz){
+    let returnNumbers = [];
+
+    for (let index = 0; index <= 100; index++) {
+        if (index % fizz == 0 && index % buzz == 0){
+            returnNumbers += index;
+        }
+    }
+    return returnNumbers;
+}
+
+/**
+ * Display output to the user
+ * View Function
+ */
+function displayResults(fizzNumbers, buzzNumbers, fizzBuzzNumbers){
+    
 }
